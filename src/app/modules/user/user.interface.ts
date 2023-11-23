6991257cfa2effe121c1b9d0217d2898f9fa200f
REFATBHUYAN <refatbubt@gmail.com>
2023-11-23 20:59:33 +0600

    create interface and user schema for users

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,30 @@
+import { Schema, model, connect } from 'mongoose';
+
+
+
+export type Product = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+
+export type User = {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: {
+    firstName: string;
+    lastName: string;
+  };
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: {
+    street: string;
+    city: string;
+    country: string;
+  };
+  orders?: Product[]
+
+}
